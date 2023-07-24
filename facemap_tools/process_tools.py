f2eb511d077821a2d7fb5656795066c8b85fd5c3
Annaaasen <65226318+Annaaasen@users.noreply.github.com>
2023-07-24 10:53:47 +0200

    Added 'facemap_tools.' to be able to import (at least on my mac)

--- facemap_tools/process_tools.py
+++ facemap_tools/process_tools.py
@@ -5,7 +5,7 @@ import os
 import sys
 sys.path.append(os.path.join(os.getcwd(), '..'))
 from facemap.process import *
-from combine_runs import combine_results_folder
+from facemap_tools.combine_runs import combine_results_folder
 
 
 
