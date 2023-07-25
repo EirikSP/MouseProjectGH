9b329723c1405c8f8bf7579e6deb58f6518b40f0
Annaaasen <65226318+Annaaasen@users.noreply.github.com>
2023-07-25 13:40:59 +0200

    Changed combine_runs to take in 'behavior_folder' as an argument. It should now work on both mac and windows.

--- facemap_tools/combine_runs.py
+++ facemap_tools/combine_runs.py
@@ -3,14 +3,14 @@ import numpy as np
 import sys
 import scipy as sp
 
-def combine(exp_names, results_folder, behaviour_base="D:\\mec-lec-por-data"):
+def combine(exp_names, results_folder, behavior_folder):
     """
     Extracts the area and area_smooth arrays from the files in the exp_names list and concatenates them.
 
     Args:
         exp_names - List of names of processed experiment files to be combined.\n
         results_folder - A global path to the folder containing the processed experiment files.\n
-        behaviour_base - The global path to the folder containing the behaviour data.\n
+        behavior_folder - The global path to the folder containing the behaviour data.\n
     
     Returns:
         complete_area - Concatenated list of all area arrays from files given in exp_names.\n
@@ -23,7 +23,7 @@ def combine(exp_names, results_folder, behaviour_base="D:\\mec-lec-por-data"):
 
     experiment_base = exp_names[0][:20]
 
-    num_points = len(np.load(os.path.join(behaviour_base, experiment_base + '_negative.npy'), allow_pickle=True))
+    num_points = len(np.load(os.path.join(behavior_folder, experiment_base + '_negative.npy'), allow_pickle=True))
     print('\Run concatenation order check: ')
     print(exp_names)
     print('\n')
@@ -44,7 +44,7 @@ def combine(exp_names, results_folder, behaviour_base="D:\\mec-lec-por-data"):
 
 
 
-def combine_results_folder(results_folder):
+def combine_results_folder(results_folder, behavior_folder="D:\\mec-lec-por-data"):
     """
     Takes a folder of processed experiment files(.npy) and extracts the area and area_smooth arrays from all files. Concatenates the arrays that are from the same run and saves them at "results_folder/run_based/{experiment_run_name: Typically like "ING7...230915_003"}_area.npy"\n
     
@@ -69,7 +69,7 @@ def combine_results_folder(results_folder):
         experiment_base = files[0][:20]
         exp_filenames = [file for file in files if file[:20] == experiment_base]
 
-        complete_area, complete_area_smooth = combine(exp_filenames, results_folder)
+        complete_area, complete_area_smooth = combine(exp_filenames, results_folder, behavior_folder)
 
         
         save_path_area = os.path.join(save_run_based, experiment_base + '_area.npy')
@@ -86,6 +86,12 @@ def combine_results_folder(results_folder):
 
 
 if __name__=='__main__':
-    combine_results_folder(sys.argv[1])
+    results_folder = sys.argv[1]
+
+    try:
+        behavior_folder = sys.argv[2]
+        combine_results_folder(results_folder, behavior_folder)
+    except:
+        combine_results_folder(results_folder)
 
     
